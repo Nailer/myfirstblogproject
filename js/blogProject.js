814cb6192967b7f924e04839f5bc8afee618fe5e
unknown <ajeemmanuel221@gmail.com>
2022-12-05 01:24:06 -1100

    site upload

---
+++ js/blogProject.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $nav = $(".navArea")
+    $toggleCollapse = $(".toggle-icons")
+
+    // click event on toggle menu
+    $toggleCollapse.click(function () {
+        $nav.toggleClass("collapse")
+    })
+
+    // owl Carousel for blog
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        
+    });
+
+
+})
