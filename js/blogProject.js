a1aab580a3b95990bf221dba2dda7b63d48edf96
Emmanuel Aje <53949905+Nailer@users.noreply.github.com>
2022-12-14 17:13:20 +0100

    The Finished Project

--- js/blogProject.js
+++ js/blogProject.js
@@ -1,3 +1,19 @@
+const responsive = {
+    0: {
+        items: 1
+    },
+
+    320: {
+        items: 1
+    },
+    560: {
+        items: 2
+    },
+    960: {
+        items: 3
+    }
+}
+
 $(document).ready(function () {
     $nav = $(".navArea")
     $toggleCollapse = $(".toggle-icons")
@@ -13,8 +29,19 @@ $(document).ready(function () {
         autoplay: true,
         autoplayTimeout: 3000,
         dots: false,
+        responsive: responsive
     });
 
+    // click to slide to the top
+
+    $(".move-up").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000)
+    })
+
+    // Animation Sliding
+    AOS.init()
 
 })
 
